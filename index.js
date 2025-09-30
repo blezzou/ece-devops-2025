7e53938f3b0cbad24394078ac682531549cfd788
Ismail CHERCHAR <100292900+Ismail-cherchar@users.noreply.github.com>
2025-09-30 14:21:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const userRouter = require('./src/routes/user');
+
+app.use(express.json());
+app.use('/user', userRouter);
+
+app.listen(3000, () => {
+  console.log('✅ Serveur démarré sur http://localhost:3000');
+});
+
+module.exports = app; // utile pour les tests
